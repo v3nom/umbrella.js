550bb412a06c5b3d9790753962b67ea868c60a90
togru <v3nomzxgt8@gmail.com>
2014-07-30 15:43:36 +0200

    add more browsers

--- karma.conf.js
+++ karma.conf.js
@@ -3,11 +3,28 @@ module.exports = function(config) {
     customLaunchers: {
       'SL_Chrome': {
         base: 'SauceLabs',
-        browserName: 'chrome'
+        browserName: 'chrome',
+        platform: 'Windows 8.1',
+        version: '36'
       },
       'SL_Firefox': {
         base: 'SauceLabs',
-        browserName: 'firefox'
+        browserName: 'firefox',
+        platform: 'Windows 8.1',
+        version: '31'
+      },
+      'SL_IE_11': {
+        base: 'SauceLabs',
+        browserName: 'internet explorer',
+        platform: 'Windows 8.1',
+        version: '11'
+      },
+      'SL_Chrome_Android': {
+        base: 'SauceLabs',
+        browserName: 'Android',
+        platform: 'Linux',
+        version: '4.4',
+        deviceName: 'Android'
       }
     },
     captureTimeout: 120000,
